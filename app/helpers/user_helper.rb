cf22e9a6417d3662e2cdc4c39ccad20642143196
Josh <jmharvey0@gmail.com>
2017-06-29 16:05:37 -0500

    testing complete of nav bar, to do styling

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,7 @@
 helpers do
-  def current_user
-    @user ||= User.find(session[:user_id]) if logged_in?
-  end
+  # def current_user
+  #   @user ||= User.find(session[:user_id]) if logged_in?
+  # end
 
   def logged_in?
     !!session[:user_id]
