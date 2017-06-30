ba5e93f8a3c1c126fe73983cc6964ffca99112e6
Matt Mayerle & Saham Khozestani <pair+mwmayerle.sahamAK@devbootcamp.com>
2017-06-30 11:06:40 -0500

    update logout to reroute to index and fixed error handling when wrong email or password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ post '/sessions' do
     session[:user_id] = @user.id
     redirect "/users/#{@user.id}"
   else
-    @errors = ["Cannot log in. Password does not match"]
+    @errors = ["Cannot log in. Please check login and password."]
     erb :'sessions/new'
   end
 end
 
 delete '/sessions' do
   session[:user_id] = nil
-  redirect '/' unless request.xhr?
+  redirect '/'
 end
