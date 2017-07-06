f05e4ecc4f62ad8962e4dd60ac7e4cc45452a0cf
Josh <jmharvey0@gmail.com>
2017-07-06 11:40:54 -0500

    add post method to create a new comment. BUG: Comments not on question show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 get '/comments/new' do
+  @commentable_type = params[:commentable_type]
+  @commentable_id = params[:commentable_id]
   erb :'comments/new'
 end
 
 post '/comments' do
+  @comment = Comment.new(params[:comment])
+  if @comment.save
+    redirect "/questions/#{session[:question_id]}"
+  else
+    @errors = ["Comments cannot be blank, Please write something"]
+    erb :'comments/new'
+  end
 
 end
